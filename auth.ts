f89250f34165fb58127747cc15f24738faf9df33
Julian Montes <julianmontes@192.168.0.3>
2024-05-22 22:06:04 -0500

    next-auth flow working

--- auth.ts
+++ auth.ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import google from "next-auth/providers/google";
+import github from "next-auth/providers/github";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Github],
+const prisma = new PrismaClient();
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [google, github],
 });
